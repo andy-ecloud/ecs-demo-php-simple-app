a644750e2fe17fed8d06095cae98e643335237a1
EC2 Default User <ec2-user@ip-10-10-0-173.us-west-2.compute.internal>
2021-08-19 08:07:48 +0000

    test

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
+                <h1>h0lA Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
@@ -23,6 +23,7 @@
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
+                        
                 ?>
 
             </div>
